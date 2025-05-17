14db2d98d911fb68bc87fa98c61515627a13547d
saransh <saranshvats041@gmail.com>
2025-05-17 12:09:35 +0530

    commit by test branch

--- file2.js
+++ file2.js
@@ -2,3 +2,7 @@ function display(){
     console.log('Enjoy to collab with Mr. Sudhir Singh!!!')
     console.log('Inside file 2')
 }
+
+function display2(){
+    console.log('added by test branch')
+}
