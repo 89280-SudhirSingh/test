502ec46811f0acc683c2528ab440da68be19b9b8
Sudhir Singh <sudhirsingh72065@gmail.com>
2025-05-17 17:28:46 +0530

    added by su

--- su1.js
+++ su1.js
@@ -3,3 +3,13 @@ console.log("hello, world")
 console.log("test, branch added")
 
 console.log("second commit by main")
+
+function mainSu(){
+    if(num%2 == 0){
+        console.log("number is even")
+    }else{
+        console.log("number is odd")
+    }
+}
+
+mainSu(10);
